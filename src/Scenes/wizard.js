6aafae65e12c6fa99de83248bc0cbfd6f4c16277
Asher <asherce21@gmail.com>
2024-06-06 16:47:10 -0700

    Update wizard.js

--- src/Scenes/wizard.js
+++ src/Scenes/wizard.js
@@ -141,7 +141,7 @@ class Wizard extends Phaser.Scene {
         //how many bullets get shot each click
         this.numBullets = 1;
         //maximum number of bullets that can be on screen at a time
-        this.maxBullets = 5;
+        this.maxBullets = 6;
         //amount of score gained per collectable pickup
         this.scoreGainPerCollectable = 10;
         this.collectableSpeed = 0;
